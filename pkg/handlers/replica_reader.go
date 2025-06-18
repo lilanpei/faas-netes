90736a9d2125fb6aca4fd9c5e2b645e3555262cb
Lanpei Li <lilanpei@gmail.com>
2025-06-18 17:20:12 +0200

    feat(faas-netes): Increase MaxReplicas/MaxFunctions and disable Dockerfile checks

--- pkg/handlers/replica_reader.go
+++ pkg/handlers/replica_reader.go
@@ -22,11 +22,11 @@ import (
 
 // MaxReplicas licensed for OpenFaaS CE is 5/5
 // a license for OpenFaaS Standard is required to increase this limit.
-const MaxReplicas = 5
+const MaxReplicas = 50
 
 // MaxFunctions licensed for OpenFaaS CE is 15
 // a license for OpenFaaS Standard is required to increase this limit.
-const MaxFunctions = 15
+const MaxFunctions = 150
 
 // MakeReplicaReader reads the amount of replicas for a deployment
 func MakeReplicaReader(defaultNamespace string, lister v1.DeploymentLister) http.HandlerFunc {
